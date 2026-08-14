a8c7efd5b5b962ad8645a48f0f312a89b950aac4
nathanielfaborada <faboradanathaniel@gmail.com>
2026-08-15 03:20:10 +0800

    gagana

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,38 +7,23 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -50,5 +35,13 @@ class User extends Authenticatable implements MustVerifyEmail
     public function events()
     {
         return $this->hasMany(Event::class);
+    }
+
+    /**
+     * Send the email verification notification using Queues.
+     */
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify((new VerifyEmail)->delay(now()->addSeconds(1)));
     }
 }
